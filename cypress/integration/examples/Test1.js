23daa79974088404f560e1299fd8e7d963ef95d2
sudhanshu.pat.kumar@gmail.com <sudhanshu.pat.kumar@gmail.com>
2024-09-12 21:34:35 +0530

    reference type commit for libraries

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,10 @@
+/// <reference types="Cypress" />
 describe('Testcases',function()
 {
-    it('First testcase',function() {
+    it('First testcase',function() 
+        {
+cy.visit("https://www.youtube.com/")
+cy.get('#search-input > #search').type('cypress')
 
-cy.visit("https://www.youtube.com/");
     })
 })
